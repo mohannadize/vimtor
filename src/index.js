730991afb4fab027bf618efd568074250448e029
Mohannad Hisham <Mohannadize15@gmail.com>
2018-10-08 22:50:46 +0000

    modified:   src/index.js 	deleted:    src/logo.svg

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+function Poop(props) {
+				return (<div>
+								<h1>{props.title}</h1>
+								<p>{props.content}</p>
+								</div>)
+}
+
 class App extends React.Component {
-  render() {
-		return (<span>Am i correct?</span>)
+    render() {
+						return (<Poop title="mohannad hisham" content="I am the biggest poop ever."/>)
 	}
 }
 ReactDOM.render(<App />, document.getElementById('root'));
