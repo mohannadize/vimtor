c35cc2e7ea389757293200a928fa722c6a057504
Mohannad Hisham <captainzebraa@pop-os.localdomain>
2018-10-14 18:25:45 +0200

    Learning vim and R.. Really enjoying it!! :D

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Poop(props) {
-				return (<div>
-								<h1>{props.title}</h1>
-								<p>{props.content}</p>
-								</div>)
+	return (<div>
+						<h1>{props.title}</h1>
+						<p>{props.content}</p>
+					</div>)
 }
 
 class App extends React.Component {
-    render() {
-						return (<Poop title="mohannad hisham" content="I am the biggest poop ever."/>)
+  render() {
+		return (<Poop title="mohannad hisham" content="I am the biggest poop ever."/>)
 	}
 }
 ReactDOM.render(<App />, document.getElementById('root'));
